8b47727c01fe5ecf35a51b3a35ab8b8ef1236cb3
Dave Hawkins <dhawkins01@gmail.com>
2019-08-10 10:07:50 -0400

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import Welcome from "./components/pages/Welcome";
 import Nav from "./components/Nav.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
         <Nav />
-        <Welcome />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          {/* <Route exact path="/books" component={Books} /> */}
+          {/* <Route exact path="/books/:id" component={Detail} /> */}
+          <Route component={NoMatch} />
+        </Switch>
       </div>
+    </Router>
       
     );
   }
